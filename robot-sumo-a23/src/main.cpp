6593e3384945a0769cdbeaa8b14e5fce2f37af5c
FelixDuck <89605334+FelixAntoine0708@users.noreply.github.com>
2023-11-01 15:38:25 -0400

    wheel front-back code

--- robot-sumo-a23/src/main.cpp
+++ robot-sumo-a23/src/main.cpp
@@ -1,33 +1,36 @@
 #include <Arduino.h>
-#include <NeoPixelBus.h>
 
-NeoPixelBus<NeoGrbwFeature, NeoSk6812Method> strip(1, 38);
+int positifA =4;
+int negatifA =1;
+int positifB = 5;
+int negatifB = 2;
 
-RgbwColor red(128, 0, 0,0);
-RgbwColor green(0, 128, 0,0);
-RgbwColor blue(0, 0, 128,0);
-
-void setup()
-{
+void setup(){
     Serial.begin(9600);
-
-    // this resets all the neopixels to an off state
-    strip.Begin();
-    strip.Show();
+    Serial.print("test");
+    pinMode(positifA, OUTPUT);
+    pinMode(negatifA, OUTPUT);
+    pinMode(positifB, OUTPUT);
+    pinMode(negatifB, OUTPUT);
 }
 
-void loop() 
-{ 
-    strip.SetPixelColor(0,red);
-    strip.Show();
-    Serial.print("Rouge\n");
-    delay(500);
-    strip.SetPixelColor(0,blue);
-    strip.Show();
-    Serial.print("Bleu\n");
-    delay(500);
-    strip.SetPixelColor(0,green);
-    strip.Show();
-    Serial.print("Vert\n");
-    delay(500);
-} 
+void forward(){
+    digitalWrite(positifA,HIGH);
+    digitalWrite(negatifA, LOW);
+    digitalWrite(positifB, LOW);
+    digitalWrite(negatifB, HIGH);
+}
+
+void backward(){
+    digitalWrite(positifA, LOW);
+    digitalWrite(negatifA,HIGH);
+    digitalWrite(positifB, HIGH);
+    digitalWrite(negatifB, LOW);
+}
+
+void loop(){
+    forward();
+    delay(1000);
+    backward();
+    delay(1000);
+}
